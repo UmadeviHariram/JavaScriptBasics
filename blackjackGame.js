e877b98d2a327a99330bebbd1dded3fb0425b62e
UmadeviHariram <umadevihariram@gmail.com>
2022-05-08 01:47:10 +0530

    blackjackgame

--- blackjackGame.js
+++ blackjackGame.js
@@ -7,10 +7,19 @@ let secondCard = getRandom(2, 11);
 let sum = fistCard + secondCard;
 console.log(fistCard, secondCard, ' ', sum)
 
-if (sum < 21) {
-    console.log("Do you want to draw a new card?")
-} else if (sum === 21) {
-    console.log("Wohoo! You've got blackjack")
-} else {
-    console.log("You're out of the game!")
+
+let result = (sum) => {
+    if (sum < 21) {
+        console.log("Do you want to draw a new card?");
+        console.log(getRandom(2, 11));
+        sum += getRandom(2, 11);
+        console.log(sum);
+        result(sum);
+    } else if (sum === 21) {
+        console.log("Wohoo! You've got blackjack")
+    } else {
+        console.log("You're out of the game!")
+    }
 }
+
+result(sum);
